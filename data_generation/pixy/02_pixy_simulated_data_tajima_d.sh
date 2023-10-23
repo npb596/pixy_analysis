859642ec58dace09d64254712593e445d03c0e1f
Nick Bailey <npb596@gmail.com>
2023-10-23 05:27:50 -0500

    Editing data generation scripts and adding Tajima's D data

--- data_generation/pixy/02_pixy_simulated_data_tajima_d.sh
+++ data_generation/pixy/02_pixy_simulated_data_tajima_d.sh
@@ -1,99 +1,115 @@
 #!/usr/bin/bash
-#SBATCH --mem=20GB
+#SBATCH -J tajima_d_pooled
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mem=2G
+#SBATCH -t 00:30:00
+#SBATCH -p jro0014_amd
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=npb0015@auburn.edu
 
+module load python/anaconda/3.8.6
+module load htslib/1.17
 
-mkdir -p tmp
-mkdir -p data
+#mkdir -p tmp
+#mkdir -p data
 
-find ../01_simulating-test-data/data/simulated_var_only -type f > tmp/vcf_var_only.txt
-find ../01_simulating-test-data/data/simulated_invar -type f > tmp/vcf_invar.txt
-find ../01_simulating-test-data/data/simulated_missing_sites -type f > tmp/vcf_missing_sites.txt
+#find ../01_simulating-test-data/data/simulated_var_only -type f > tmp/vcf_var_only.txt
+#find ../01_simulating-test-data/data/simulated_invar -type f > tmp/vcf_invar.txt
+#find ../01_simulating-test-data/data/simulated_missing_sites -type f > tmp/vcf_missing_sites.txt
 find ../01_simulating-test-data/data/simulated_missing_genos -type f > tmp/vcf_missing_genos.txt
-find ../01_simulating-test-data/data/accuracy_invar -type f > tmp/vcf_accuracy.txt
+#find ../01_simulating-test-data/data/accuracy_invar -type f > tmp/vcf_accuracy.txt
 
-##rm -r data/var_only
-##mkdir -p data/var_only
-#
+#rm -r data/var_only
+#mkdir -p data/var_only
+
 #while read vcf
 #do
-#
+
 #mkdir -p tmp/1
 #rm -r tmp/1
 #vcfslug=$(echo $vcf | sed 's/.*\///g')
 #echo $vcf
-#
-#python pixy.py --stats tajima_d --vcf $vcf --zarr_path tmp/1 --window_size 10000 --populations populations_pi.txt --bypass_filtration yes --outfile_prefix data/var_only/$vcfslug
-#
+#bgzip $vcf
+#tabix -p vcf ${vcf}.gz
+#python ../../../../pixy/pixy/__main__.py --stats tajima_d --vcf ${vcf}.gz --window_size 10000 --populations populations_pi.txt --bypass_invariant_check yes --output_folder data/var_only/ --output_prefix $vcfslug
+
 #rm -r tmp/1
-#
+
 #done < tmp/vcf_var_only.txt 
-#
+
 ##rm -r data/invar
-##mkdir -p data/invar
-#
+#mkdir -p data/invar
+
 #while read vcf
 #do
-#
+
 #mkdir -p tmp/1
 #rm -r tmp/1
 #vcfslug=$(echo $vcf | sed 's/.*\///g')
 #echo $vcf
-#
-#python pixy.py --stats tajima_d --vcf $vcf --zarr_path tmp/1 --window_size 10000 --populations populations_pi.txt --bypass_filtration yes --outfile_prefix data/invar/$vcfslug
-#
+#bgzip $vcf
+#tabix -p vcf ${vcf}.gz
+#python ../../../../pixy/pixy/__main__.py --stats tajima_d --vcf ${vcf}.gz --window_size 10000 --populations populations_pi.txt --bypass_invariant_check yes --output_folder data/invar/ --output_prefix $vcfslug
+
 #rm -r tmp/1  
-#
+
 #done < tmp/vcf_invar.txt
-#
+
 ##rm -r data/missing_sites
-##mkdir -p data/missing_sites
-#
+#mkdir -p data/missing_sites
+
 #while read vcf
 #do
-#
+
 #mkdir -p tmp/1
 #rm -r tmp/1
 #vcfslug=$(echo $vcf | sed 's/.*\///g')
 #echo $vcf
-#
-#python pixy.py --stats tajima_d --vcf $vcf --zarr_path tmp/1 --window_size 10000 --populations populations_pi.txt --bypass_filtration yes --outfile_prefix data/missing_sites/$vcfslug
-#
+#bgzip $vcf
+#tabix -p vcf ${vcf}.gz
+#python ../../../../pixy/pixy/__main__.py --stats tajima_d --vcf ${vcf}.gz --window_size 10000 --populations populations_pi.txt --bypass_invariant_check yes --output_folder data/missing_sites/ --output_prefix $vcfslug
+
 #rm -r tmp/1  
-#
+
 #done < tmp/vcf_missing_sites.txt
-#
-#
+
+
 ##rm -r data/missing_genos
-##mkdir -p data/missing_genos
-#
-#while read vcf
-#do
-#
-#mkdir -p tmp/1
-#rm -r tmp/1
-#vcfslug=$(echo $vcf | sed 's/.*\///g')
-#echo $vcf
-#
-#python pixy.py --stats tajima_d --vcf $vcf --zarr_path tmp/1 --window_size 10000 --populations populations_pi.txt --bypass_filtration yes --outfile_prefix data/missing_genos/$vcfslug
-#
-#rm -r tmp/1   
-#
-#done < tmp/vcf_missing_genos.txt
+#mkdir -p data/missing_genos
 
-
-
-mkdir -p data/accuracy_invar
 while read vcf
 do
 
 mkdir -p tmp/1
-rm -r tmp/1
+#rm -r tmp/1
 vcfslug=$(echo $vcf | sed 's/.*\///g')
-echo $vcf
+#echo $vcf
+if ( file $vcf | grep -v "compressed" )
+then 
+bgzip $vcf
+tabix -p vcf ${vcf}.gz
+python ../../../../pixy/pixy/__main__.py --stats tajima_d --vcf ${vcf}.gz --window_size 10000 --populations populations_pi.txt --bypass_invariant_check yes --output_folder data/missing_genos/ --output_prefix $vcfslug
+fi
 
-python pixy.py --stats tajima_d --vcf $vcf --zarr_path tmp/1 --window_size 10000 --populations populations_pi.txt --bypass_filtration yes --outfile_prefix data/accuracy_invar/$vcfslug
+#rm -r tmp/1   
 
-rm -r tmp/1   
+done < tmp/vcf_missing_genos.txt
 
-done < tmp/vcf_accuracy.txt
+
+
+#mkdir -p data/accuracy_invar
+#while read vcf
+#do
+
+#mkdir -p tmp/1
+#rm -r tmp/1
+#vcfslug=$(echo $vcf | sed 's/.*\///g')
+#echo $vcf
+
+#python pixy.py --stats tajima_d --vcf $vcf --zarr_path tmp/1 --window_size 10000 --populations populations_pi.txt --bypass_invariant_check yes --output_prefix data/accuracy_invar/$vcfslug
+
+#rm -r tmp/1   
+
+#done < tmp/vcf_accuracy.txt
 
--- data_generation/pixy/05_aggregate_results.R
+++ data_generation/pixy/05_aggregate_results.R
@@ -7,25 +7,288 @@ library("ggdark")
 
 # pi
 
-pi_files <- list.files("data/missing_genos", full.names = TRUE, pattern = ".*pi.txt")
-pi_files <- c(pi_files, list.files("data/missing_sites", full.names = TRUE, pattern = ".*pi.txt"))
-pi_files <- c(pi_files, list.files("data/accuracy_invar", full.names = TRUE, pattern = ".*pi.txt"))
+#pi_files <- list.files("data/missing_genos", full.names = TRUE, pattern = ".*pi.txt")
+#pi_files <- c(pi_files, list.files("data/missing_sites", full.names = TRUE, pattern = ".*pi.txt"))
+#pi_files <- c(pi_files, list.files("data/accuracy_invar", full.names = TRUE, pattern = ".*pi.txt"))
 
 
 # expected pi
+#Ne <- 1e6
+#Mu <- 1e-8
+#exp_pi <- 4*Ne*Mu
+
+# sample size
+#n <- 50
+
+# li and nei 1975
+# expected variance over all loci
+#v_pi <- (2*exp_pi*(exp_pi+4)+8*(n-1)*exp_pi)/(2*n*(2*n-1)*(exp_pi+1)*(exp_pi+2)*(exp_pi+3))
+
+
+#read_pi <- function(x){
+  
+  #cat(paste0(x, "\n"))
+  
+#  df <- try({df <- read.table(x, h = T)}, silent = TRUE)
+#  if(class(df) == "data.frame"){
+    
+#    if(nrow(df) > 0){
+      
+#      data.frame(filename = x, df)
+      
+#    }
+    
+#  }
+  
+#}
+
+#pixy_pi <- lapply(pi_files, read_pi)
+#pixy_pi <- bind_rows(pixy_pi)
+
+#pixy_pi <- pixy_pi %>%
+#  mutate(missing_type = ifelse(grepl("genos", filename), "genotypes", "sites")) %>%
+#  mutate(missing_type = ifelse(grepl("accuracy", filename), "accuracy", missing_type)) %>%
+#  mutate(missing_data = ifelse(grepl("genos", filename), 
+#                               as.numeric(gsub(".*missing_genos=|.vcf.*", "", filename)),
+#                               as.numeric(gsub(".*missing_|.vcf.*", "", filename)))) %>%
+#  mutate(missing_data = ifelse(missing_type == "sites", (10000-missing_data)/10000, missing_data))%>%
+#  mutate(missing_data = ifelse(missing_type == "accuracy", 0, missing_data))
+
+# this is the maxmium value of pi for a VCF
+# i.e. the "true" per site estimate of pi for that sample
+# with zero missing data
+# used for scaling
+#max_pixy <- pixy_pi  %>%
+#  filter(missing_type == "sites"|missing_type == "accuracy") %>%
+#  mutate(vcf_source = gsub("_invar.missing.*|_invar.vcf.*", "", filename) %>% gsub(".*/", "", .)) %>%
+#  filter(missing_data == 0) %>%
+#  mutate(max_pi_pixy = avg_pi) %>% 
+#  select(vcf_source,  pop, max_pi_pixy) %>%
+#  arrange(vcf_source)
+
+#pixy_pi <- pixy_pi %>%
+#  mutate(vcf_source = gsub("_invar.missing.*|_invar.vcf.*", "", filename) %>% gsub(".*/", "", .)) %>%
+#  left_join(max_pixy) %>% 
+#  mutate(pi_scaled = avg_pi/max_pi_pixy) 
+
+# inspect the data
+#pixy_pi %>%
+#  ggplot(aes(x = missing_data, y = pi_scaled))+
+#  geom_point(size = 1, alpha = 0.5)+
+#  geom_hline(yintercept = 1, color = "red", size = 0.75, linetype = 1) +
+#  facet_wrap(~missing_type)+
+#  xlab("Proportion of Data Missing")+
+#  ylab("Scaled Pi Estimate") +
+  #scale_color_viridis_c()+
+#  dark_theme_gray(base_size = 20)+
+#  theme(axis.line = element_line(colour = "grey50"),
+#        panel.grid = element_line(colour = "grey15"))+
+#  scale_x_continuous(breaks = scales::pretty_breaks(n = 6)) +
+#  scale_y_continuous(breaks = scales::pretty_breaks(n = 6))
+
+# check the accuracy data
+#avg_pi_obs <- pixy_pi %>%
+#  filter(missing_type == "accuracy") %>%
+#  filter(!is.na(avg_pi)) %>%
+#  pull(avg_pi) %>%
+#  mean
+
+#avg_pi_obs
+#exp_pi
+
+# check the accuracy data
+#var_pi_obs <- pixy_pi %>%
+#  filter(missing_type == "accuracy") %>%
+#  filter(!is.na(avg_pi)) %>%
+#  pull(avg_pi) %>%
+#  var
+
+#var_pi_obs
+#v_pi
+
+
+# dxy
+#dxy_files <- list.files("data/missing_genos", full.names = TRUE, pattern = ".*dxy.txt")
+#dxy_files <- c(dxy_files, list.files("data/missing_sites", full.names = TRUE, pattern = ".*dxy.txt"))
+#dxy_files <- c(dxy_files, list.files("data/accuracy_invar", full.names = TRUE, pattern = ".*dxy.txt"))
+
+# expected pi
+#Ne <- 1e6
+#Mu <- 1e-8
+#exp_pi <- 4*Ne*Mu
+#exp_pi <- (12*Ne*Mu)/(3+(16*Ne*Mu))
+
+#pixy_dxy <- lapply(dxy_files, read_pi)
+#pixy_dxy <- bind_rows(pixy_dxy)
+
+#pixy_dxy <- pixy_dxy %>%
+#  mutate(missing_type = ifelse(grepl("genos", filename), "genotypes", "sites")) %>%
+#  mutate(missing_type = ifelse(grepl("accuracy", filename), "accuracy", missing_type)) %>%
+#  mutate(missing_data = ifelse(grepl("genos", filename), 
+#                               as.numeric(gsub(".*missing_genos=|.vcf.*", "", filename)),
+#                               as.numeric(gsub(".*missing_|.vcf.*", "", filename)))) %>%
+#  mutate(missing_data = ifelse(missing_type == "sites", (10000-missing_data)/10000, missing_data)) %>%
+#  mutate(missing_data = ifelse(missing_type == "accuracy", 0, missing_data))
+
+# this is the maxmium value of pi for a VCF
+# i.e. the "true" per site estimate of pi for that sample
+# with zero missing data
+# used for scaling
+#max_dxy <- pixy_dxy %>%
+#  filter(missing_type == "sites"|missing_type == "accuracy") %>%
+#  mutate(vcf_source = gsub("_invar.missing.*|_invar.vcf.*", "", filename) %>% gsub(".*/", "", .)) %>%
+#  filter(missing_data == 0) %>% 
+#  mutate(max_dxy_pixy = avg_dxy) %>% 
+#  select(vcf_source, max_dxy_pixy) %>%
+#  arrange(vcf_source)
+
+#pixy_dxy <- pixy_dxy %>%
+#  mutate(vcf_source = gsub("_invar.missing.*|_invar.vcf.*", "", filename) %>% gsub(".*/", "", .)) %>%
+#  left_join(max_dxy) %>%
+#  mutate(dxy_scaled = avg_dxy/max_dxy_pixy)
+
+# inspect the data
+#pixy_dxy %>%
+#  ggplot(aes(x = missing_data, y = dxy_scaled))+
+#  geom_point(size = 1, alpha = 0.5)+
+#  geom_hline(yintercept = 1, color = "red", size = 0.75, linetype = 1) +
+#  facet_wrap(~missing_type)+
+#  xlab("Proportion of Data Missing")+
+#  ylab("Scaled Pi Estimate") +
+  #scale_color_viridis_c()+
+#  dark_theme_gray(base_size = 20)+
+#  theme(axis.line = element_line(colour = "grey50"),
+#        panel.grid = element_line(colour = "grey15"))+
+#  scale_x_continuous(breaks = scales::pretty_breaks(n = 6)) +
+#  scale_y_continuous(breaks = scales::pretty_breaks(n = 6))
+
+#pixy_dat <- pixy_dxy %>%
+#  select(vcf_source, missing_type, missing_data, avg_dxy) %>%
+#  left_join(pixy_pi, .) %>%
+#  mutate(method = "pixy") %>%
+#  select(vcf_source, missing_type, missing_data, method, avg_pi, avg_dxy)
+
+# Watterson's Theta
+
+#theta_files <- list.files("data/missing_genos", full.names = TRUE, pattern = ".*theta.txt")
+#theta_files <- c(pi_files, list.files("data/missing_sites", full.names = TRUE, pattern = ".*theta.txt"))
+#theta_files <- c(pi_files, list.files("data/accuracy_invar", full.names = TRUE, pattern = ".*theta.txt"))
+
+
+# expected Watterson's theta
+#Ne <- 1e6
+#Mu <- 1e-8
+#exp_theta <- 4*Ne*Mu
+
+# sample size
+#n <- 50
+
+# li and nei 1975
+# expected variance over all loci
+#v_theta <- (2*exp_theta*(exp_theta+4)+8*(n-1)*exp_theta)/(2*n*(2*n-1)*(exp_theta+1)*(exp_theta+2)*(exp_theta+3))
+
+
+#read_theta <- function(x){
+  
+  #cat(paste0(x, "\n"))
+  
+#  df <- try({df <- read.table(x, h = T)}, silent = TRUE)
+#  if(class(df) == "data.frame"){
+    
+#    if(nrow(df) > 0){
+      
+#      data.frame(filename = x, df)
+      
+#    }
+    
+#  }
+  
+#}
+
+#pixy_theta <- lapply(theta_files, read_theta)
+#pixy_theta <- bind_rows(pixy_theta)
+
+#pixy_theta <- pixy_theta %>%
+#  mutate(missing_type = ifelse(grepl("genos", filename), "genotypes", "sites")) %>%
+#  mutate(missing_type = ifelse(grepl("accuracy", filename), "accuracy", missing_type)) %>%
+#  mutate(missing_data = ifelse(grepl("genos", filename), 
+#                               as.numeric(gsub(".*missing_genos=|.vcf.*", "", filename)),
+#                               as.numeric(gsub(".*missing_|.vcf.*", "", filename)))) %>%
+#  mutate(missing_data = ifelse(missing_type == "sites", (10000-missing_data)/10000, missing_data))%>%
+#  mutate(missing_data = ifelse(missing_type == "accuracy", 0, missing_data))
+
+# this is the maxmium value of Watterson's theta for a VCF
+# i.e. the "true" per site estimate of Watterson's theta for that sample
+# with zero missing data
+# used for scaling
+#max_pixy <- pixy_theta  %>%
+#  filter(missing_type == "sites"|missing_type == "accuracy") %>%
+#  mutate(vcf_source = gsub("_invar.missing.*|_invar.vcf.*", "", filename) %>% gsub(".*/", "", .)) %>%
+#  filter(missing_data == 0) %>%
+#  mutate(max_theta_pixy = avg_theta) %>% 
+#  select(vcf_source,  pop, max_theta_pixy) %>%
+#  arrange(vcf_source)
+
+#pixy_theta <- pixy_theta %>%
+#  mutate(vcf_source = gsub("_invar.missing.*|_invar.vcf.*", "", filename) %>% gsub(".*/", "", .)) %>%
+#  left_join(max_pixy) %>% 
+#  mutate(theta_scaled = avg_pi/max_theta_pixy) 
+
+# inspect the data
+#pixy_theta %>%
+#  ggplot(aes(x = missing_data, y = theta_scaled))+
+#  geom_point(size = 1, alpha = 0.5)+
+#  geom_hline(yintercept = 1, color = "red", size = 0.75, linetype = 1) +
+#  facet_wrap(~missing_type)+
+#  xlab("Proportion of Data Missing")+
+#  ylab("Scaled Watterson's Theta Estimate") +
+  #scale_color_viridis_c()+
+#  dark_theme_gray(base_size = 20)+
+#  theme(axis.line = element_line(colour = "grey50"),
+#        panel.grid = element_line(colour = "grey15"))+
+#  scale_x_continuous(breaks = scales::pretty_breaks(n = 6)) +
+#  scale_y_continuous(breaks = scales::pretty_breaks(n = 6))
+
+# check the accuracy data
+#avg_theta_obs <- pixy_theta %>%
+#  filter(missing_type == "accuracy") %>%
+#  filter(!is.na(avg_theta)) %>%
+#  pull(avg_theta) %>%
+#  mean
+
+#avg_theta_obs
+#exp_theta
+
+# check the accuracy data
+#var_pi_obs <- pixy_theta %>%
+#  filter(missing_type == "accuracy") %>%
+#  filter(!is.na(avg_theta)) %>%
+#  pull(avg_theta) %>%
+#  var
+
+#var_theta_obs
+#v_theta
+
+# Tajima's D
+
+tajima_files <- list.files("data/missing_genos", full.names = TRUE, pattern = ".*tajima_d.txt")
+tajima_files <- c(tajima_files, list.files("data/missing_sites", full.names = TRUE, pattern = ".*tajima_d.txt"))
+tajima_files <- c(tajima_files, list.files("data/accuracy_invar", full.names = TRUE, pattern = ".*tajima_d.txt"))
+
+# expected Tajima's D
 Ne <- 1e6
 Mu <- 1e-8
-exp_pi <- 4*Ne*Mu
+exp_tajima <- 0
 
 # sample size
 n <- 50
 
 # li and nei 1975
 # expected variance over all loci
-v_pi <- (2*exp_pi*(exp_pi+4)+8*(n-1)*exp_pi)/(2*n*(2*n-1)*(exp_pi+1)*(exp_pi+2)*(exp_pi+3))
+v_pi <- (2*exp_tajima*(exp_tajima+4)+8*(n-1)*exp_tajima)/(2*n*(2*n-1)*(exp_tajima+1)*(exp_tajima+2)*(exp_tajima+3))
 
 
-read_pi <- function(x){
+read_tajima <- function(x){
   
   #cat(paste0(x, "\n"))
   
@@ -42,43 +305,41 @@ read_pi <- function(x){
   
 }
 
-pixy_pi <- lapply(pi_files, read_pi)
-pixy_pi <- bind_rows(pixy_pi)
-
-pixy_pi <- pixy_pi %>%
-  mutate(missing_type = ifelse(grepl("genos", filename), "genotypes", "sites")) %>%
-  mutate(missing_type = ifelse(grepl("accuracy", filename), "accuracy", missing_type)) %>%
-  mutate(missing_data = ifelse(grepl("genos", filename), 
-                               as.numeric(gsub(".*missing_genos=|.vcf.*", "", filename)),
-                               as.numeric(gsub(".*missing_|.vcf.*", "", filename)))) %>%
+pixy_tajima <- lapply(tajima_files, read_tajima)
+pixy_tajima <- bind_rows(pixy_tajima)
+head(pixy_tajima)
+pixy_tajima <- pixy_tajima %>% mutate(missing_type = ifelse(grepl("genos", filename), "genotypes", "sites")) %>% 
+         mutate(missing_type = ifelse(grepl("accuracy", filename), "accuracy", missing_type)) %>%
+         mutate(missing_data = ifelse(grepl("genos", filename),
+			as.numeric(gsub(".*missing_genos=|.vcf.*", "", filename)),
+                        as.numeric(gsub(".*missing_|.vcf.*", "", filename)))) %>%
   mutate(missing_data = ifelse(missing_type == "sites", (10000-missing_data)/10000, missing_data))%>%
   mutate(missing_data = ifelse(missing_type == "accuracy", 0, missing_data))
-
-# this is the maxmium value of pi for a VCF
-# i.e. the "true" per site estimate of pi for that sample
+head(pixy_tajima)
+# this is the maximum value of Tajima's D for a VCF
 # with zero missing data
 # used for scaling
-max_pixy <- pixy_pi  %>%
+max_pixy <- pixy_tajima  %>%
   filter(missing_type == "sites"|missing_type == "accuracy") %>%
   mutate(vcf_source = gsub("_invar.missing.*|_invar.vcf.*", "", filename) %>% gsub(".*/", "", .)) %>%
   filter(missing_data == 0) %>%
-  mutate(max_pi_pixy = avg_pi) %>% 
-  select(vcf_source,  pop, max_pi_pixy) %>%
+  mutate(max_tajima_pixy = tajima_d) %>% 
+  select(vcf_source,  pop, max_tajima_pixy) %>%
   arrange(vcf_source)
 
-pixy_pi <- pixy_pi %>%
+pixy_tajima <- pixy_tajima %>%
   mutate(vcf_source = gsub("_invar.missing.*|_invar.vcf.*", "", filename) %>% gsub(".*/", "", .)) %>%
   left_join(max_pixy) %>% 
-  mutate(pi_scaled = avg_pi/max_pi_pixy) 
+  mutate(tajima_scaled = tajima_d/max_tajima_pixy) 
 
 # inspect the data
-pixy_pi %>%
-  ggplot(aes(x = missing_data, y = pi_scaled))+
+pixy_tajima %>%
+  ggplot(aes(x = missing_data, y = tajima_d))+
   geom_point(size = 1, alpha = 0.5)+
   geom_hline(yintercept = 1, color = "red", size = 0.75, linetype = 1) +
   facet_wrap(~missing_type)+
   xlab("Proportion of Data Missing")+
-  ylab("Scaled Pi Estimate") +
+  ylab("Tajima's D Estimate") +
   #scale_color_viridis_c()+
   dark_theme_gray(base_size = 20)+
   theme(axis.line = element_line(colour = "grey50"),
@@ -87,85 +348,29 @@ pixy_pi %>%
   scale_y_continuous(breaks = scales::pretty_breaks(n = 6))
 
 # check the accuracy data
-avg_pi_obs <- pixy_pi %>%
-  filter(missing_type == "accuracy") %>%
-  filter(!is.na(avg_pi)) %>%
-  pull(avg_pi) %>%
-  mean
+#tajima_obs <- pixy_tajima %>%
+#  filter(missing_type == "accuracy") %>%
+#  filter(!is.na(tajima)) %>%
+#  pull(tajima) %>%
+#  mean
 
-avg_pi_obs
-exp_pi
+#avg_pi_obs
+#exp_pi
 
 # check the accuracy data
-var_pi_obs <- pixy_pi %>%
-  filter(missing_type == "accuracy") %>%
-  filter(!is.na(avg_pi)) %>%
-  pull(avg_pi) %>%
-  var
+#var_pi_obs <- pixy_pi %>%
+#  filter(missing_type == "accuracy") %>%
+#  filter(!is.na(avg_pi)) %>%
+#  pull(avg_pi) %>%
+#  var
 
-var_pi_obs
-v_pi
+#var_pi_obs
+#v_pi
 
+#pixy_dat <- pixy_tajima %>%
+#  select(vcf_source, missing_type, missing_data, avg_dxy) %>%
+#  left_join(pixy_pi, .) %>%
+#  mutate(method = "pixy") %>%
+#  select(vcf_source, missing_type, missing_data, method, tajima_d)
 
-# dxy
-dxy_files <- list.files("data/missing_genos", full.names = TRUE, pattern = ".*dxy.txt")
-dxy_files <- c(dxy_files, list.files("data/missing_sites", full.names = TRUE, pattern = ".*dxy.txt"))
-dxy_files <- c(dxy_files, list.files("data/accuracy_invar", full.names = TRUE, pattern = ".*dxy.txt"))
-
-# expected pi
-Ne <- 1e6
-Mu <- 1e-8
-#exp_pi <- 4*Ne*Mu
-exp_pi <- (12*Ne*Mu)/(3+(16*Ne*Mu))
-
-pixy_dxy <- lapply(dxy_files, read_pi)
-pixy_dxy <- bind_rows(pixy_dxy)
-
-pixy_dxy <- pixy_dxy %>%
-  mutate(missing_type = ifelse(grepl("genos", filename), "genotypes", "sites")) %>%
-  mutate(missing_type = ifelse(grepl("accuracy", filename), "accuracy", missing_type)) %>%
-  mutate(missing_data = ifelse(grepl("genos", filename), 
-                               as.numeric(gsub(".*missing_genos=|.vcf.*", "", filename)),
-                               as.numeric(gsub(".*missing_|.vcf.*", "", filename)))) %>%
-  mutate(missing_data = ifelse(missing_type == "sites", (10000-missing_data)/10000, missing_data)) %>%
-  mutate(missing_data = ifelse(missing_type == "accuracy", 0, missing_data))
-
-# this is the maxmium value of pi for a VCF
-# i.e. the "true" per site estimate of pi for that sample
-# with zero missing data
-# used for scaling
-max_dxy <- pixy_dxy %>%
-  filter(missing_type == "sites"|missing_type == "accuracy") %>%
-  mutate(vcf_source = gsub("_invar.missing.*|_invar.vcf.*", "", filename) %>% gsub(".*/", "", .)) %>%
-  filter(missing_data == 0) %>% 
-  mutate(max_dxy_pixy = avg_dxy) %>% 
-  select(vcf_source, max_dxy_pixy) %>%
-  arrange(vcf_source)
-
-pixy_dxy <- pixy_dxy %>%
-  mutate(vcf_source = gsub("_invar.missing.*|_invar.vcf.*", "", filename) %>% gsub(".*/", "", .)) %>%
-  left_join(max_dxy) %>%
-  mutate(dxy_scaled = avg_dxy/max_dxy_pixy)
-
-# inspect the data
-pixy_dxy %>%
-  ggplot(aes(x = missing_data, y = dxy_scaled))+
-  geom_point(size = 1, alpha = 0.5)+
-  geom_hline(yintercept = 1, color = "red", size = 0.75, linetype = 1) +
-  facet_wrap(~missing_type)+
-  xlab("Proportion of Data Missing")+
-  ylab("Scaled Pi Estimate") +
-  #scale_color_viridis_c()+
-  dark_theme_gray(base_size = 20)+
-  theme(axis.line = element_line(colour = "grey50"),
-        panel.grid = element_line(colour = "grey15"))+
-  scale_x_continuous(breaks = scales::pretty_breaks(n = 6)) +
-  scale_y_continuous(breaks = scales::pretty_breaks(n = 6))
-
-pixy_dat <- pixy_dxy %>%
-  select(vcf_source, missing_type, missing_data, avg_dxy) %>%
-  left_join(pixy_pi, .) %>%
-  mutate(method = "pixy") %>%
-  select(vcf_source, missing_type, missing_data, method, avg_pi, avg_dxy)
-
-write_rds(pixy_dat, "data/pixy_simulated_data.rds")
+write_rds(pixy_tajima, "data/pixy_simulated_data.rds")
