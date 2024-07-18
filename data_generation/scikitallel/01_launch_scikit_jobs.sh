35a7ef8b3a51406938909192a21b5ad53b4a058c
Nicholas Bailey <npb596@gmail.com>
2024-07-18 15:14:49 +0200

    Modify scripts to run on invariant data

--- data_generation/scikitallel/01_launch_scikit_jobs.sh
+++ data_generation/scikitallel/01_launch_scikit_jobs.sh
@@ -3,37 +3,37 @@
 #SBATCH -J scikit_allel
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --mem=5G
-#SBATCH -t 6-00:00:00
+#SBATCH --mem=3G
+#SBATCH -t 1-00:00:00
 #SBATCH -p normal
 #SBATCH -e scikit_allel.err
 #SBATCH -o scikit_allel.out
 
-rm data/pi_out/*
-rm data/dxy_out/*
-rm data/watterson_theta_out/*
-rm data/tajima_d_out/*
+#rm data/pi_out/*
+#rm data/dxy_out/*
+#rm data/watterson_theta_out/*
+#rm data/tajima_d_out/*
 
-ls -d ../01_simulating-test-data/data/simulated_missing_genos/* > tmp/missing_genos_dir_list.txt
-ls -d ../01_simulating-test-data/data/simulated_missing_sites/* > tmp/missing_sites_dir_list.txt
+#ls -d ../01_simulating-test-data/data/simulated_missing_genos/* > tmp/missing_genos_dir_list.txt
+#ls -d ../01_simulating-test-data/data/simulated_missing_sites/* > tmp/missing_sites_dir_list.txt
 #
-while read vcffolder 
-do
+#while read vcffolder 
+#do
 # 
-./02_scikit_dxy_pi_vcfolder.sh $vcffolder
+#./02_scikit_dxy_pi_wt_td_vcfolder.sh $vcffolder
 # 
-done < tmp/missing_genos_dir_list.txt
+#done < tmp/missing_genos_dir_list.txt
 #
 #
-while read vcffolder 
-do
+#while read vcffolder 
+#do
 #
-./02_scikit_dxy_pi_vcfolder.sh $vcffolder
+#./02_scikit_dxy_pi_wt_td_vcfolder.sh $vcffolder
 #
-done < tmp/missing_sites_dir_list.txt
+#done < tmp/missing_sites_dir_list.txt
 #
 #
-#sbatch 02_scikit_dxy_pi_vcfolder.sh ../01_simulating-test-data/data/simulated_invar
+sbatch 02_scikit_dxy_pi_wt_td_vcfolder.sh ../01_simulating-test-data/data/simulated_invar
 
 
-#sbatch 02_scikit_dxy_pi_vcfolder.sh ../simulating-test-data/data/accuracy_invar
+#sbatch 02_scikit_dxy_pi_wt_td_vcfolder.sh ../simulating-test-data/data/accuracy_invar
--- data_generation/vcftools/03_vcftools_simulated_data_tajima_d.sh
+++ data_generation/vcftools/03_vcftools_simulated_data_tajima_d.sh
@@ -3,8 +3,8 @@
 #SBATCH -J vcftools
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --mem=5G
-#SBATCH -t 6-00:00:00
+#SBATCH --mem=3G
+#SBATCH -t 1-00:00:00
 #SBATCH -p normal
 #SBATCH -e vcftools.err
 #SBATCH -o vcftools.out
@@ -12,14 +12,14 @@
 ##############################################################################################
 # Runs vcftools --TajimaD on the BFS population
 
-mkdir -p tmp
-mkdir -p data
+#mkdir -p tmp
+#mkdir -p data
 
-find ../01_simulating-test-data/data/simulated_var_only -type f > tmp/vcf_var_only.txt
+#find ../01_simulating-test-data/data/simulated_var_only -type f > tmp/vcf_var_only.txt
 find ../01_simulating-test-data/data/simulated_invar -type f > tmp/vcf_invar.txt
-find ../01_simulating-test-data/data/simulated_missing_sites -type f > tmp/vcf_missing_sites.txt
-find ../01_simulating-test-data/data/simulated_missing_genos -type f > tmp/vcf_missing_genos.txt
-find ../01_simulating-test-data/data/accuracy_invar -type f > tmp/vcf_accuracy.txt
+#find ../01_simulating-test-data/data/simulated_missing_sites -type f > tmp/vcf_missing_sites.txt
+#find ../01_simulating-test-data/data/simulated_missing_genos -type f > tmp/vcf_missing_genos.txt
+#find ../01_simulating-test-data/data/accuracy_invar -type f > tmp/vcf_accuracy.txt
 
 #mkdir -p data/var_only
 #
@@ -32,53 +32,53 @@ find ../01_simulating-test-data/data/accuracy_invar -type f > tmp/vcf_accuracy.t
 #done < tmp/vcf_var_only.txt 
 #
 #
-#mkdir -p data/invar
+mkdir -p data/invar
+#
+while read vcf
+do
+#
+vcfslug=$(echo $vcf | sed 's/.*\///g')
+#
+vcftools --gzvcf $vcf --TajimaD 10000 --chr 1 --out data/invar/$vcfslug
+#
+done < tmp/vcf_invar.txt
+#
+#
+#
+#mkdir -p data/missing_sites
 #
 #while read vcf
 #do
 #
 #vcfslug=$(echo $vcf | sed 's/.*\///g')
+#/beegfs/data/soft/vcftools/bin/vcftools --gzvcf $vcf --TajimaD 10000 --chr 1 --out data/missing_sites/$vcfslug
 #
-#vcftools --gzvcf $vcf --TajimaD 10000 --chr 1 --out data/invar/$vcfslug
-#
-#done < tmp/vcf_invar.txt
-#
-#
-#
-mkdir -p data/missing_sites
-#
-while read vcf
-do
-#
-vcfslug=$(echo $vcf | sed 's/.*\///g')
-/beegfs/data/soft/vcftools/bin/vcftools --gzvcf $vcf --TajimaD 10000 --chr 1 --out data/missing_sites/$vcfslug
-#
-done < tmp/vcf_missing_sites.txt
+#done < tmp/vcf_missing_sites.txt
 #
 #
 #
-mkdir -p data/missing_genos
+#mkdir -p data/missing_genos
 #
-while read vcf
-do
+#while read vcf
+#do
 #
-vcfslug=$(echo $vcf | sed 's/.*\///g')
-/beegfs/data/soft/vcftools/bin/vcftools --gzvcf $vcf --TajimaD 10000 --chr 1 --out data/missing_genos/$vcfslug
+#vcfslug=$(echo $vcf | sed 's/.*\///g')
+#/beegfs/data/soft/vcftools/bin/vcftools --gzvcf $vcf --TajimaD 10000 --chr 1 --out data/missing_genos/$vcfslug
 #
-done < tmp/vcf_missing_genos.txt
+#done < tmp/vcf_missing_genos.txt
 
 
 
 
 
-mkdir -p data/accuracy_invar
+#mkdir -p data/accuracy_invar
 
-while read vcf
-do
+#while read vcf
+#do
 
-vcfslug=$(echo $vcf | sed 's/.*\///g')
-vcftools --gzvcf $vcf --TajimaD 10000 --chr 1 --out data/accuracy_invar/$vcfslug
+#vcfslug=$(echo $vcf | sed 's/.*\///g')
+#vcftools --gzvcf $vcf --TajimaD 10000 --chr 1 --out data/accuracy_invar/$vcfslug
 
-done < tmp/vcf_accuracy.txt
+#done < tmp/vcf_accuracy.txt
 
 
